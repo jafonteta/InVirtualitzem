f3283a2d0ba539c747d179a64b4a5cc7791f971e
jafonteta <ja.moragarcia@edu.gva.es>
2023-02-03 08:15:29 +0100

    Prototipo Alpha 0.1
    Están enlazadas las escenas. Mejorado sistema de carga, con fader entre escenas. Implementado sistema de audio ambiental por escenas. Tablet de menú principal funcionando, incluida los toggles de localización. Nuevos prefabs de XR origin, etc. 3 tipos de tablets (main, para escenas segundarias y para terciarias). Nueva escena de coger cacharrito. Implementado localización de un audio con éxito (nueva tabla, audio event).

--- CellAnatomy/Assets/Scripts/CambioEscena.cs
+++ CellAnatomy/Assets/Scripts/CambioEscena.cs
@@ -8,15 +8,58 @@ public class CambioEscena : MonoBehaviour
     [SerializeField]
     protected EasyAudioUtility_SceneManager miEasyAudio;
 
+    GameObject faderObject;
+
+    [Header("ESTO SE ASIGNARÁ SOLO")]
+    Animator faderAnimator;
+
     private void Start()
     {
-        miEasyAudio = FindObjectOfType<EasyAudioUtility_SceneManager>();
+        if(faderAnimator == null)
+        {
+            CheckForAnimator();
+        }
+        
+       
     }
     public void CargarEscena(string sceneName)
     {
+        if (faderAnimator == null)
+        {
+            faderObject = GameObject.FindGameObjectWithTag("Fader");
+            faderAnimator = faderObject.GetComponent<Animator>();
+        }
+        if(faderAnimator != null)
+        {
+            faderAnimator.SetTrigger("FadeIn");
+        }
+ 
         SceneManager.LoadScene(sceneName);
-        miEasyAudio.onSceneChange(sceneName);
+        
+        if(miEasyAudio == null)
+        {
+            CheckForAudioEsceneManager();
+        }
+        if(miEasyAudio != null)
+        {
+            miEasyAudio.onSceneChange(sceneName);
+        }
+        if (faderAnimator != null)
+        {
+            faderAnimator.SetTrigger("FadeOut");
+        }
+        
     }
 
+    void CheckForAnimator()
+    {
+        faderObject = GameObject.FindGameObjectWithTag("Fader");
+        faderAnimator = faderObject.GetComponent<Animator>();
+    }
+
+    void CheckForAudioEsceneManager()
+    {
+        miEasyAudio = FindObjectOfType<EasyAudioUtility_SceneManager>();
+    }
     
 }
--- CellAnatomy/Assets/Scripts/PasaMenuInvocacion.cs
+++ CellAnatomy/Assets/Scripts/PasaMenuInvocacion.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PasaMenu : MonoBehaviour
+public class PasaMenuInvocacion : MonoBehaviour
 {
     [SerializeField]
     protected EasyAudioUtility_SceneManager miEasyAudio;
+
+    [SerializeField]
+    protected Animator miAnimation;
 
     public string nombreEscenaMenu = "Escena_02_Menu";
     // Start is called before the first frame update
@@ -23,8 +26,10 @@ public class PasaMenu : MonoBehaviour
     }
     public void PasarEscena()
     {
+        miAnimation.SetTrigger("FadeIn");
         SceneManager.LoadScene(nombreEscenaMenu);
         miEasyAudio.onSceneChange(nombreEscenaMenu);
+        miAnimation.SetTrigger("FadeOut");
     }
 
    
