7dec15d90ffde184995a372e26c5cd6773c10159
JesusFonteta <jesgonini@alu.edu.gva.es>
2023-02-24 10:48:59 +0100

    Prefabs Tablets
    tablets y script salida

---
+++ CellAnatomy/Assets/Scripts/ExitJuego.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitJuego : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    public void CerrarAplicacion()
+    {
+        Application.Quit();
+    }
+
+
+}
